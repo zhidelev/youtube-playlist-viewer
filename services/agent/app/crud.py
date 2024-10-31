da65e75f8513e8ad954fae98160bf559c4ea9d7f
Andrei Zhidelev <mcjanz@gmail.com>
2024-10-31 14:08:46 +0300

    feat: forbid adding duplicates (#7)
    * feat: forbid adding duplicates
    
    * fix: add tests

--- services/agent/app/crud.py
+++ services/agent/app/crud.py
@@ -3,7 +3,12 @@ from sqlalchemy.orm import Session
 from . import models, schemas
 
 
-def create_item(db: Session, item: schemas.Item):
+def create_item(db: Session, item: schemas.TrimmedItem):
+    # Checking if item is already in the database
+    result = db.query(models.Item).filter(models.Item.list == item.list).first()
+    if result:
+        return result
+
     db_item = models.Item(list=item.list, processed=False)
     db.add(db_item)
     db.commit()
@@ -13,3 +18,7 @@ def create_item(db: Session, item: schemas.Item):
 
 def get_items(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.Item).offset(skip).limit(limit).all()
+
+
+def get_item(db: Session, item_id):
+    return db.query(models.Item).filter(models.Item.id == item_id).first()
--- services/agent/app/test_endpoints.py
+++ services/agent/app/test_endpoints.py
@@ -1,22 +1,11 @@
 import pytest
-import schemathesis
 from fastapi.testclient import TestClient
 
 from .main import app
 
-# Globally enable OpenAPI 3.1 experimental feature
-schemathesis.experimental.OPEN_API_3_1.enable()
-
-
 client = TestClient(app)
 
 pytestmark = pytest.mark.api
-
-
-# def test_today():
-#     response = client.get("/today")
-#     assert response.status_code == 200
-#     assert response.json()
 
 
 @pytest.fixture
@@ -39,6 +28,27 @@ def test_add_content(create_list_item):
     assert resp["list"] == "PL0MRiRrXAvRhuVf-g4o3IO0jmpLQgubZK"
 
 
+def test_add_content_duplicates():
+    response_initial = client.post(
+        "/data", json={"url": "https://youtube.com/playlist?list=PL0MRiRrXAvRhuVf-g4o3IO0jmpLQgubZL"}
+    )
+    assert response_initial.status_code == 200, response_initial.text
+    resp = response_initial.json()
+    assert "id" in resp
+    assert resp["id"] > 0
+    initial_id = resp["id"]
+    assert "processed" in resp
+    assert resp["processed"] is False
+    assert "list" in resp
+    assert resp["list"] == "PL0MRiRrXAvRhuVf-g4o3IO0jmpLQgubZL"
+
+    response_duplicate = client.post(
+        "/data", json={"url": "https://youtube.com/playlist?list=PL0MRiRrXAvRhuVf-g4o3IO0jmpLQgubZL"}
+    )
+    assert response_duplicate.status_code == 200, response_duplicate.text
+    assert response_duplicate.json()["id"] == initial_id
+
+
 @pytest.mark.parametrize("skip,limit", [(0, 10), (1, 1), (0, 1)])
 def test_list_content(create_list_item, skip, limit):
     response = client.get("/lists", params={"skip": skip, "limit": limit})
@@ -59,14 +69,3 @@ def test_list_content(create_list_item, skip, limit):
 def test_list_content_negative(create_list_item, skip, limit):
     response = client.get("/lists", params={"skip": skip, "limit": limit})
     assert response.status_code == 422
-
-
-schema = schemathesis.from_uri(
-    "http://127.0.0.1:8000/openapi.json",
-)
-
-
-@pytest.mark.auto_generated
-@schema.parametrize()
-def test_api(case):
-    case.call_and_validate()
